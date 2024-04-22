3fe87fe12afaa8ba992716238892df533aefb2ee
Michael Töpfer <micha.toepfer@googlemail.com>
2024-04-22 12:08:00 +0200

    fetch agains the api and create card

--- components/CharacterCard/CharacterCard.js
+++ components/CharacterCard/CharacterCard.js
@@ -1,1 +1,28 @@
-export function CharacterCard() {}
+export default function CharacterCard(character) {
+  const { image, name, status, species, episode } = character;
+  const episodeLength = character.episode.length;
+  const card = document.createElement("li");
+  card.classList.add("card");
+
+  card.innerHTML = `<div class="card__image-container">
+    <img
+      class="card__image"
+      src="${image}"
+      alt=">${name}"
+    />
+    <div class="card__image-gradient"></div>
+  </div>
+  <div class="card__content">
+    <h2 class="card__title">${name}</h2>
+    <dl class="card__info">
+      <dt class="card__info-title">Status</dt>
+      <dd class="card__info-description">${status}</dd>
+      <dt class="card__info-title">Type</dt>
+      <dd class="card__info-description">${species}</dd>
+      <dt class="card__info-title">Occurrences</dt>
+      <dd class="card__info-description">${episodeLength}</dd>
+    </dl>
+  </div>`;
+
+  return card;
+}
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import CharacterCard from "./components/CharacterCard/CharacterCard.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +14,30 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+const baseURL = "https://rickandmortyapi.com/api/character";
+
+async function fetchCharacters(name = "") {
+  const url = `${baseURL}?page=${page}&name=${encodeURIComponent(name)}`;
+
+  try {
+    const response = await fetch(baseURL);
+    const { results, info } = await response.json();
+
+    cardContainer.innerHTML = "";
+
+    results.forEach((character) => {
+      const card = CharacterCard(character);
+      cardContainer.appendChild(card);
+    });
+
+    console.log(results);
+
+    // updatePagination(info);
+  } catch (error) {
+    console.error("Error fetching characters: ", error);
+  }
+}
+
+// Call fetchCharacters initially to load the first page
+fetchCharacters();
