e58f7a2ea0610ee88c9d2dbb04ceba8555505908
Eva Bergold <evabergold@gmail.com>
2024-04-22 15:31:01 +0200

    optimize pagination

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import CharacterCard from "./components/CharacterCard/CharacterCard.js";
-import Pagination from "./components/NavPagination/NavPagination.js";
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
@@ -7,32 +6,58 @@ const searchBarContainer = document.querySelector(
 );
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
-//folgende zeilen hab ich in NavPagination.js verschoben:
-//const prevButton = document.querySelector('[data-js="button-prev"]');
-//const nextButton = document.querySelector('[data-js="button-next"]');
-//const pagination = document.querySelector('[data-js="pagination"]');
+
+const prevButton = document.querySelector('[data-js="button-prev"]');
+const nextButton = document.querySelector('[data-js="button-next"]');
+const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-// const maxPage = 1;
+let maxPage = 1;
 let page = 1;
 const searchQuery = "";
 
-export async function fetchCharacters(page) {
+async function fetchCharacters(page) {
   try {
     const apiURL = `https://rickandmortyapi.com/api/character?page=${page}`;
     const response = await fetch(apiURL);
-    const { results } = await response.json();
-
-    cardContainer.innerHTML = "";
-    results.forEach((character) => {
-      const card = CharacterCard(character);
-      cardContainer.append(card);
-    });
-
-    console.log(results);
+    const { info, results } = await response.json();
+    maxPage = info.pages;
+    pagination.textContent = page + " / " + maxPage;
+    renderCharacters(results);
+    console.log(info, results);
   } catch (error) {
     console.error("Error fetching characters", error);
   }
 }
 
+function renderCharacters(characters) {
+  cardContainer.innerHTML = "";
+  characters.forEach((character) => {
+    const card = CharacterCard(character);
+    cardContainer.append(card);
+  });
+}
+
 fetchCharacters(page);
+
+prevButton.addEventListener("click", (event) => {
+  console.log("prev button clicked");
+  if (page > 1) {
+    page--;
+    console.log("page:", page);
+    fetchCharacters(page);
+    pagination.textContent = page + " / " + maxPage;
+    console.log("pagination value: ", page);
+  }
+});
+
+nextButton.addEventListener("click", (event) => {
+  console.log("next button clicked");
+  if (page < maxPage) {
+    page++;
+    console.log("page:", page);
+    fetchCharacters(page);
+    pagination.textContent = page + " / " + maxPage;
+    console.log("pagination value: ", page);
+  }
+});
